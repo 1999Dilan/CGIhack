89801836350f40330cf256df3c8b98024f51baed
vRimuru-sama <62886531+vRimuru-sama@users.noreply.github.com>
2020-04-01 14:22:59 +0700

    CGIhack
    Sayangin game kalian dari pada ngecheat !!

---
+++ CGI.py
@@ -0,0 +1,64 @@
+print ("""---------------------------------""")
+print ("""pesan author = gunakan ya awaslu""")
+print ("""---------------------------------""") 
+
+
+print ("masukan pasword =vRimuru")
+input ("masukan pw di atas =")
+
+
+i = 1
+while i <= 20:
+  print('tunggu nyet..')
+  i += 1
+
+
+
+print ("selamat lu dah masuk kontol!")
+
+
+print("=====================================")
+print("||AUTHOR =vRimuru-sama||=============")
+print("||Team   =vRnekopoi.  ||=============")
+print("||tools hack  game    ||=============")
+print("||baca bismilah dulu  ||=============")
+print("=====================================")
+input ("ketik bismilah =")
+print("""------------------------------------""")
+class Hacktool:
+ def Hack():
+   print("""
+pilih memek!!:
+""")
+   print("()1)duit banyak!!    (2) aku gratis!")
+   anw = int(input())
+ def Loading():
+  i = 0
+  while i <= 100:
+    print(i)
+    i= i+1
+    if i==101:
+     print("bagus lu niat bet") 
+     print("lu ketipu sama gw dah pergi lah! awkokakwo😁😁😂")   
+ def Failed():
+   print("""
+  maaf bro lu salah ketik jomblo seh""")
+       
+print("""-----------hack game lu tools-----------""")
+print("            author = vRimuru-sama         ")
+print("""
+pesan = pilih game lu yang mau lu hack gooblox!
+  """)
+print("(1)Pubg.   <------>   (2)Mobile Legends  ")
+print("(3) exit aja bro")
+gamech = int(input())
+
+try:
+ if gamech==1:
+  Hacktool.Hack()
+  Hacktool.Loading()
+ if gamech==2:
+  Hacktool.Hack()
+  Hacktool.Loading()
+except:
+  Hacktool.Failed()
